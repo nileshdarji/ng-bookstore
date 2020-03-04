f0c172ef84f18021c1fd711717c39a7bbf119b2e
nileshdarji <nileshdarji@localhost>
2020-03-03 23:24:42 -0800

    Initial commit with spring boot initializer application and application properties

---
+++ ngbookstore/src/test/java/com/learning/ngbookstore/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.ngbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
