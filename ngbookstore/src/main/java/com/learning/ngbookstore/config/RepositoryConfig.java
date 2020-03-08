da2a9aa8783de06a57e3b969352c2c2c99bf90a5
nileshdarji <nileshdarji@localhost>
2020-03-07 18:22:00 -0800

    Implemented Rest services for book and book-category

---
+++ ngbookstore/src/main/java/com/learning/ngbookstore/config/RepositoryConfig.java
@@ -0,0 +1,27 @@
+package com.learning.ngbookstore.config;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+	
+	@Autowired
+	private EntityManager entityManage;
+	
+	@Override
+	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+		//config.exposeIdsFor(Book.class);
+		
+		//Expose ids for all the entities
+		config.exposeIdsFor(entityManage.getMetamodel().getEntities().stream()
+				.map(Type::getJavaType)
+				.toArray(Class[]::new));
+	}
+	
+}
