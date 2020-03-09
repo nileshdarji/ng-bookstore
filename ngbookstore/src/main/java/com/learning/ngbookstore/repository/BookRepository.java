7f4c5a78ab11b6b207e21d004eb77d044f160b9e
nileshdarji <nileshdarji@localhost>
2020-03-08 21:14:18 -0700

    Initial commit for the Angular project

--- ngbookstore/src/main/java/com/learning/ngbookstore/repository/BookRepository.java
+++ ngbookstore/src/main/java/com/learning/ngbookstore/repository/BookRepository.java
@@ -1,8 +1,11 @@
 package com.learning.ngbookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 import com.learning.ngbookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")  // Or "*" to allow any request
 public interface BookRepository extends JpaRepository<Book, Long> {
 
 }
